62b4119918e13198d5b2f28d1133e2681c9b4067
jihanejador <jihanejador@gmail.com>
2026-04-24 15:13:03 +0100

    Ajout de la logique de connexion avec vérification du mot de passe et gestion des sessions

--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -22,3 +22,28 @@ if (isset($_POST['register'])){
         header("Location: ../public/login.php?success=1");
     }
 }
+if (isset($_POST['login'])){
+
+    $email = cleanInput($_POST['email']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM user WHERE email = '$email'";
+    $result = $xonn->query($sql);
+
+    if ($result->num_rows == 1){
+        $user = $result->ftech_assoc();
+
+        if (password_verify($password,$user['password'])){
+
+        $_SESSION['user']=$user['firstname'];
+        $_SESSION['role'] = $user['role'];
+
+        header("Location: ../public/dashboard.php");
+        } else{
+            header("Location: ../public/login.php?error=invalid");
+        }
+    } else {
+        header("Location: ../public/login.php?error=invalid");
+    }
+}
+?>
