64a43f776b53ebc6ac4490704911e7e98352cf27
jihanejador <jihanejador@gmail.com>
2026-04-24 15:27:44 +0100

    Mise en place de la connexion à la base de données avec PDO

--- connection.php
+++ connection.php
@@ -3,3 +3,9 @@ $server="localhost";
 $dbname="edusync";
 $dbuser="root";
 $dbpasw = "";
+try {
+    $conn=new PDO("mysql:host=$server;dbname=$dbname", $dbuser, $dbpasw);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
